5930b0f378d992b00bf3c54847d7a890d991f35d
Iman Ghasrfakhri <ghasrfakhri@gmail.com>
2021-06-08 18:35:10 +0430

    Init Commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$transport = (new Swift_SmtpTransport('mail.roshdana.i-gh.ir', 465))
+    ->setUsername('info@roshdana.i-gh.ir')
+    ->setPassword('ZPMR@YHFs}z&');
+
+$mailer = new Swift_Mailer($transport);
+
+
+
+//$message = new Swift_Message('Wonderful Subject');
+//$message->setFrom(['john@doe.com' => 'John Doe']);
+//$message->setTo(['receiver@domain.org', 'other@domain.org' => 'A name']);
+//$message->setBody('Here is the message itself');
+
+$message = (new Swift_Message('ُSalam From Roshdana'))
+    ->setFrom(['info@roshdana.i-gh.ir' => 'Roshdana'])
+    ->setTo(['ghasrfakhri@gmail.com'])
+    ->setBody('Matne Payam');
+
+
+if($mailer->send($message) > 0 ){
+    echo "Sent.";
+}
